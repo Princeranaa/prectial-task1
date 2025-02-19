b42d34c60a8ed5ec8fedde4e21d66db886ef46cc
Prince Rana <ranaprince45may@gmail.com>
2025-02-19 10:15:44 +0530

    `Update form actions and select options in user details and login pages, and update admin router imports.`

--- router/AdminRouter.js
+++ router/AdminRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { handleLogin, logout, updateWalletBalance } = require('../Controller/adminController');
-const { getAllUsers, updateUserStatus, getUserDetails } = require('../Controller/User-Controller');
+const { handleLogin, logout, updateWalletBalance, getUserDetails } = require('../Controller/adminController');
+const { getAllUsers, updateUserStatus } = require('../Controller/User-Controller');
 
 // Login page
 router.get('/login', (req, res) => {
